4fe44de2560857453bc277d1a9c756bc2715541a
Aleksandar Trajkov <aletrajko@foi.hr>
2018-01-17 00:13:58 +0100

    Formatiranje ispisa
    fi

--- Forecast_Russia2018/F2R18.cs
+++ Forecast_Russia2018/F2R18.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,42 +16,41 @@ namespace Forecast_Russia2018
         {
             InitializeComponent();
             Creator c = new Creator();
-            Team t = new Team();
+            Team tt = new Team();
             DB db = DB.getInstance();
             c.createState();
             c.createPlayers();
             c.createMatches();
-            t.calculateForm();
+            tt.calculateForm();
             db.distribute();
 
 
 
-             foreach (Group g in db.giveGroups())
-            {
-                tbRezultati.Text += g.rows.Count() + Environment.NewLine;
-
-                foreach (Team tt in db.giveTeams())
-                {
-                    tbRezultati.Text += tt.name + " ";
-                    tbRezultati.Text += tt.form + Environment.NewLine;
-
-                }
             
-            foreach (Group g in db.giveGroups()) {
-                
-                tbRezultati.Text += "Group " + g.name + Environment.NewLine;
-                
-               foreach (GroupRow t in g.rows) {
-                    if (t.team.name.Length < 6)  tbRezultati.Text += t.team.name + "\t\t\t" + t.points.ToString() + Environment.NewLine;
-                    else if (t.team.name.Length <= 6 && t.team.name.Length < 11) tbRezultati.Text += t.team.name + "\t\t" + t.points.ToString() + Environment.NewLine;
-                    else tbRezultati.Text += t.team.name + "\t" + t.points.ToString() + Environment.NewLine; 
+                foreach (Group g in db.giveGroups())
+                {
+
+                    tbRezultati.Text += "[Group " + g.name + "]" + Environment.NewLine + "--------------------------" + Environment.NewLine;
+                    
+                    foreach (GroupRow t in g.rows)
+                    {
+                    if(t.team.name.Length < 5) tbRezultati.Text += t.team.name + "\t\t\t\t" + t.points.ToString() + Environment.NewLine;
+                    else if (t.team.name.Length >= 5 && t.team.name.Length < 10) tbRezultati.Text += t.team.name + "\t\t\t" + t.points.ToString() + Environment.NewLine;
+                    else tbRezultati.Text += t.team.name + "\t\t" + t.points.ToString() + Environment.NewLine;
+
+                    /*
+                        if (t.team.name.Length < 6) tbRezultati.Text += t.team.name + "\t\t\t" + t.points.ToString() + Environment.NewLine;
+                        else if (t.team.name.Length <= 6 && t.team.name.Length < 11) tbRezultati.Text += t.team.name + "\t\t" + t.points.ToString() + Environment.NewLine;
+                        else tbRezultati.Text += t.team.name + "\t" + t.points.ToString() + Environment.NewLine;
+                        */
                 }
-                tbRezultati.Text += Environment.NewLine;
+                    tbRezultati.Text += Environment.NewLine;
+                }
+
+
             }
 
 
+        }
     }
 
-
-    }
-}
